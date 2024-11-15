bf6bf35135ffc122077c8ff50586bf7127ea0a50
KJM <jmkang1984@gmail.com>
2024-11-15 14:45:13 +0900

    클래스문의 - 클래스 id에 맞는거 당겨오기 완료

--- Learn_On/src/main/java/com/itwillbs/learnon/HomeController.java
+++ Learn_On/src/main/java/com/itwillbs/learnon/HomeController.java
@@ -2,20 +2,30 @@ package com.itwillbs.learnon;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.itwillbs.learnon.service.CourseService;
+import com.itwillbs.learnon.vo.CommonCodeTypeVO;
+import com.itwillbs.learnon.vo.CourseVO;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	
+	@Autowired
+	CourseService courseService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -24,8 +34,18 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		List<CommonCodeTypeVO> codeTypeAll = courseService.getCodeTypeAll();
+		model.addAttribute("codeTypeAll", codeTypeAll);
+
+		
 		// index.jsp 페이지로 포워딩
 		return "index";
+	}
+	
+	public String commonCodeList(Model model, CourseVO course) {
+		
+		
+		return "inc/top";
 	}
 	
 }
